e18fa588c415411ac6272c59f3c7df8866cdd86b
Hoffmann-Ferreira <andreihf@outlook.com>
2022-06-26 20:31:47 -0300

    feat: validation of the data in the routers

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsNumber, IsString } from 'class-validator';
+
 export class CreatUserDto {
+  @IsString()
   name: string;
+  @IsEmail()
   email: string;
+  @IsNumber()
   password: number;
 }
