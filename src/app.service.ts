efb81b4cee31132267037ac534a810666137289a
Hoffmann-Ferreira <andreihf@outlook.com>
2022-06-27 17:26:01 -0300

    feat: documentation and its settings

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'App running 👨‍🚀! Visit http://localhost:333/docs for documentatio';
+    return 'App running 👨‍🚀! Visit http://localhost:3333/docs/ for documentatio';
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreatUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
