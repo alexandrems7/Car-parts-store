847b40c6c83e086f9a66dbbdd41f2c5cc5bc3b9b
Hoffmann-Ferreira <andreihf@outlook.com>
2022-07-03 16:54:13 -0300

    feat: new products module includes module, controller and service

---
+++ src/products/products.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('products')
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'register new proudct' })
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'list all products present in the database' })
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'search pruduct by id' })
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'update a product' })
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(+id, updateProductDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'delete a product' })
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
+}
