ebba7672ab50d687064892a1fcc5e0538ec7bce1
Hoffmann-Ferreira <andreihf@outlook.com>
2022-07-12 22:29:20 -0300

    feat: method creat order

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,10 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { domainToASCII } from 'url';
 import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
 export class OrdersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    const data: Prisma.OrderCreateInput = {
+      table: {
+        connect: {
+          number: createOrderDto.tableNumber,
+        },
+      },
+      user: {
+        connect: {
+          id: createOrderDto.userId,
+        },
+      },
+      products: {
+        connect: createOrderDto.products.map((element) => ({ id: element })),
+      },
+    };
+
+    return this.prisma.order.create({
+      data,
+      select: {
+        id: true,
+        tableNumber: true,
+        userId: true,
+        createdAt: true,
+        products: { select: { name: true } },
+      },
+    });
   }
 
   findAll() {
